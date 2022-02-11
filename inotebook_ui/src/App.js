3637e7ce42a0a8cf23ce3c664941ae89f1cfae99
Govind Mishra <govindkm.sv@gmail.com>
2022-02-11 10:57:37 +0530

    Router Working

--- inotebook_ui/src/App.js
+++ inotebook_ui/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./components/Home";
-import { About } from "./components/About";
-import { Navbar } from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="">
-      <Navbar></Navbar>
+    <>
       <Router>
+        <Navbar></Navbar>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
